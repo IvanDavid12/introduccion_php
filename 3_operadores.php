60a0628606169ba103372840f6f6ce1c0352421e
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-07-17 18:41:08 -0500

    php

---
+++ 3_operadores.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+
+    <h1>operadores</h1>
+    <?php
+    //operadores aritmeticos
+    //operadores de comparacion
+    //operadores logicos
+    //operadores de union de cadenas
+    ?>
+    <h1>operadores aritmeticos</h1>
+    <?php
+
+    $num1 = 10;
+    $num2 = 5;
+
+
+    $suma = $num1 + $num2;
+
+    echo "<p>Primer número: $num1</p>";
+    echo "<p>Segundo número: $num2</p>";
+    echo "<h2>Resultados:</h2>";
+    echo "<p>Suma: $suma</p>";
+    ?>
+
+<h1>Operadores de Comparación</h1>
+
+<?php
+$num1 = 10;
+$num2 = 5;
+
+$igual = ($num1 == $num2);
+$distinto = ($num1 != $num2);
+$mayor = ($num1 > $num2);
+$menor = ($num1 < $num2);
+$mayorIgual = ($num1 >= $num2);
+$menorIgual = ($num1 <= $num2);
+
+
+?>
+
+   
+
+</body>
+</html>
