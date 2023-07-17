d436172aeea595657c600d6492fb0becfb8c2154
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-07-17 17:24:49 -0500

    php

---
+++ 1_comentarios.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>comentarios en php</h1>
+    <!---formas de incluir codigo php--->
+    <?php
+        //comentario de una linea/*esto es un comentaro de varias lineas*/
+    ?>
+     <!---
+    <%
+        codigo php
+    %>
+    <script lenguaje="php">
+
+    </script>--->
+</body>
+</html>
