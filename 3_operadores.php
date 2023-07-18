36479f91a96f4a2b1941364af4b6e31dfe5d79c0
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-07-18 07:56:17 -0500

    php

--- 3_operadores.php
+++ 3_operadores.php
@@ -17,17 +17,82 @@
     ?>
     <h1>operadores aritmeticos</h1>
     <?php
+        $num1 = 1;
+        echo ("x = ".$num1."<br>");
+        $num2 = 5;
+        echo ("x = ".$num2."<br>");
+        $num3 = 4;
+        echo ("x = ".$num3."<br>");
+
+        $suma = $num1 + $num2;
+        echo ("suma = ".$suma."<br>");
+        $resta = $num1 - $num3;
+        echo ("resta = ".$resta."<br>");
+        $multiplicacion = $suma*$resta;
+        echo ("multiplicacion = ".$multiplicacion."<br>");
+        $final = $multiplicacion++;
+        echo("final =".$final."<br>");
+  
+    ?>
+    <?php
+    echo"<h1>operadores de comparacion</h1><br>";
+    $x = 5;
+    $y = 5;
+    echo ($x == $y);
+    echo ($x <= $y);
+    ?>
+    <?php
+    echo"<h1>operadores logicos</h1><br>";
+    $x = 5;
+    $y = 5;
+
+    if(($x==4) && ($y==5));
+    {
+        print("estas en lo correcto");
+    }
+    echo"<br>";
+    if(($x==4) OR ($y==3));
+    {
+        print("la segunda estas en lo correcto");
+    }
+    ?>
+    <?php
+    echo "<h1>Operadores de union de cadenas</h1><br>";
+    $t = "Ejemplo";
+    $w = 'union';
+    $x = "de";
+    $y = "cadenas";
+    $z = ' ';
+    $resultado1 = $t;
+    $resultado2 = $t.$z.$x.$z.$w.$z.$x.$z.$y;
+    echo '<b><h1>';
+    echo $resultado1;
+    echo "<hr>";
+    echo $resultado2;
+    echo "</b></hr>";
+    ?>
+
+<h1>------------------------------------------------</h1>
+<h1>operadores aritmeticos</h1>
+    <?php
 
     $num1 = 1;
     $num2 = 5;
 
-
     $suma = $num1 + $num2;
+    $resta = $num1 - $num2;
+    $multiplicacion = $num1 * $num2;
+    $division = $num1 / $num2;
+
 
     echo "<p>Primer número: $num1</p>";
     echo "<p>Segundo número: $num2</p>";
     echo "<h2>Resultados:</h2>";
     echo "<p>Suma: $suma</p>";
+    echo "<p>Resta: $resta</p>";
+    echo "<p>Multiplicación: $multiplicacion</p>";
+    echo "<p>División: $division</p>";
+
     ?>
 
 <h1>Operadores de Comparación</h1>
---
+++ 4_estructura_control.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>estructura de control</h1>
+    <?php
+    // if
+    // if-else
+    // while
+    // do-while
+    // for
+    // require
+    // include
+    // switch
+    ?>
+</body>
+</html>
