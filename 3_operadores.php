0ae0f74a1303ffb034a1fb56cf48cede524e853b
Chalarcanicolas <nicolaschalarca@colegioguanenta.edu.co>
2023-07-18 06:39:50 -0500

    php

--- 3_operadores.php
+++ 3_operadores.php
@@ -18,7 +18,7 @@
     <h1>operadores aritmeticos</h1>
     <?php
 
-    $num1 = 10;
+    $num1 = 1;
     $num2 = 5;
 
 
@@ -33,20 +33,42 @@
 <h1>Operadores de Comparación</h1>
 
 <?php
-$num1 = 10;
+$num1 = 5;
 $num2 = 5;
 
 $igual = ($num1 == $num2);
 $distinto = ($num1 != $num2);
 $mayor = ($num1 > $num2);
 $menor = ($num1 < $num2);
-$mayorIgual = ($num1 >= $num2);
-$menorIgual = ($num1 <= $num2);
 
 
-?>
+    echo "<p>Primer número: $num1</p>";
+    echo "<p>Segundo número: $num2</p>";
+    echo "<h2>Resultados:</h2>";
+    echo "<p>igual: " . ($igual ? 'verdadero' : 'falso') . "</p>";
+    echo "<p>diferentes: " . ($distinto ? 'verdadero' : 'falso') . "</p>";
+    echo "<p>mayor: " . ($mayor ? 'verdadero' : 'falso') . "</p>";
+    echo "<p>menor: " . ($menor ? 'verdadero' : 'falso') . "</p>";
+    ?>
 
+<h1>operadores logicos</h1>
+
+
+<h1>operadores de union de cadenas</h1>
+
+<?php
+    $cadena_1 = "Hola";
+    $cadena_2 = "mundo";
+
+    $union = $cadena_1 . " " . $cadena_2;
+
+    echo "<p>palabra 1: $cadena_1</p>";
+    echo "<p>palabra 2: $cadena_2</p>";
+    echo "<h2>Resultado de la unión:</h2>";
+    echo "<p>union : $union</p>";
+    ?>
    
+
 
 </body>
 </html>
